09bfa48187ebb42fe9f170f2f0982e90ac8f49d3
vincentpaulzapanta <vincentpaulzapanta@gmail.com>
2024-04-16 17:07:03 +0800

    Added data, images and script for education

---
+++ data.js
@@ -0,0 +1,33 @@
+
+
+const dbti ={
+	image: "./images/dbti-logo.png",
+	name:"Don Bosco Technical Institute Makati",
+	description:"Don bosco"
+
+}
+
+
+const ust ={
+	image: "./images/ust-logo.png",
+	name:"University Of Santo Tomas",
+	description:"Ust chuchuchuchcu"
+
+}
+
+
+
+const rivan ={
+	image: "./images/rivan-logo.png",
+	name:"Rivan Technology Institute",
+	description:"Rivan"
+
+}
+
+
+const zuitt ={
+	image: "./images/zuitt-logo.png",
+	name:"Zuitt Coding Bootcamp",
+	description:"Zuitt"
+
+}
