6a0c8a580e99dd6058bf87ccae44f0570232f3cd
vincentpaulzapanta <vincentpaulzapanta@gmail.com>
2024-04-17 12:00:40 +0800

    Added CSS animations for education

--- data.js
+++ data.js
@@ -3,7 +3,7 @@
 const dbti ={
 	image: "./images/dbti-logo.png",
 	name:"Don Bosco Technical Institute Makati",
-	description:"Don bosco"
+	description:"I fulfilled my elementary and highschool education from 2003 to 2013. The curriculum focused on the usual subjects but had a larger empahsis in technical subjects during the highchool years. Subjects such as computer, drafting, mechanical and automotive were tackled during those years. I specialized in automotive technology during my junior and senior years "
 
 }
 
@@ -11,7 +11,7 @@ const dbti ={
 const ust ={
 	image: "./images/ust-logo.png",
 	name:"University Of Santo Tomas",
-	description:"Ust chuchuchuchcu"
+	description:"The course i took was B.S. Occupational therapy from 2013-2018. The course focuses on the analysis of tasks and client factors in order to help achieve the optimal level of functioning for people with physical and/or cognitive disabilites"
 
 }
 
@@ -20,7 +20,7 @@ const ust ={
 const rivan ={
 	image: "./images/rivan-logo.png",
 	name:"Rivan Technology Institute",
-	description:"Rivan"
+	description:"I took a Bootcamp that focused on studying for the CCNA. Skills that were focused on were Subnetting and Routing Protocols. Inrtoduction to cloud and vritual machines were also given."
 
 }
 
@@ -28,6 +28,9 @@ const rivan ={
 const zuitt ={
 	image: "./images/zuitt-logo.png",
 	name:"Zuitt Coding Bootcamp",
-	description:"Zuitt"
+	description:"The bootcamp covered front end, back end, and Fullstack development. We used HTML, CSS, Javascript and, the MERN stack in order to learn programming and create websites."
 
 }
+
+
+
